360f395386913d23558ca10ccc0cb739b2f77786
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2025-12-07 10:01:05 -0300

    pop up completo

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,11 @@
-const agendar = document.querySelector('#button')
+const agendar = document.querySelector('.button-new-schedules')
 const modalContainer = document.querySelector('#modal-container')
-
+const closeButton = document.querySelector('.close-button')
 
 agendar.addEventListener('click', () => {
     modalContainer.showModal()
+})
+
+closeButton.addEventListener('click', () => {
+    modalContainer.close()
 })
